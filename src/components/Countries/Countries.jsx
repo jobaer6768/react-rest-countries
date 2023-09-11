4f460397d4abbca841aacf757c6ad01c0d82c1fc
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-09-11 11:28:52 +0600

    made some changes on visited-btn

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './countries.css'
 
 const Countries = () => {
 
@@ -15,9 +16,11 @@ const Countries = () => {
     return (
         <div>
             <h5>Countries: {countries.length}</h5>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+            <div className="country-container">
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,26 @@
-import './box.css'
+import { useState } from 'react';
+import './country.css'
 
 const Country = ({country}) => {
     console.log(country)
 
-    const {name, flags} = country;
+    const {name, flags, capital} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
 
     return (
-        <div className="box">
-            <h6>Name: {name.common}</h6>
-            <img src={flags.png} alt={flags.alt} />
+        <div className={`country ${visited ? 'visited box' : 'box'}`}>
+            <h3>Name: {name.common}</h3>
+            <h5>Capital: {capital}</h5>
+            <img className='img-width' src={flags.png} alt={flags.alt} />
+            <button onClick={handleVisited} className='btn-visited'>Visited</button>
+            {
+                <p>{visited ? 'Already Visited' : 'Have to go there'}</p>
+            }
         </div>
     );
 };
